66268f640bb6033f90d0478d05bad6dabc0d0cfe
incopro-zhongyuan <zhongyuan.zhang@incopro.co.uk>
2015-03-24 11:53:32 +0000

    Update statements.py
    This was changed because the save will fail when trying to save a list/map of items (except string).

--- cqlengine/statements.py
+++ cqlengine/statements.py
@@ -79,7 +79,7 @@ class BaseClause(UnicodeMixin):
     def update_context(self, ctx):
         """ updates the query context with this clauses values """
         assert isinstance(ctx, dict)
-        ctx[str(self.context_id)] = self.value
+        ctx[str(self.context_id)] = self.value.value if hasattr(self.value, 'value') else self.value
 
 
 class WhereClause(BaseClause):
